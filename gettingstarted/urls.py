2bdce71f559b8db04ed251d117095cd321af62b5
SilentWrangler <thousandgoblins@gmail.com>
2019-08-15 11:02:35 +0300

    Setting ip store

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,6 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    path("regip/", hello.views.updip, name="regip"),
     path("admin/", admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
+    HTTP_409_CONFLICT
+)
+from rest_framework.response import Response
+
+from .models import IPStore
 
 # Create your views here.
+@api_view(["GET"])
+@permission_classes((AllowAny,))
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    ip = None
+
+    try:
+        ip = IPStore.ojects.get().ip
+    except IPStore.DoesNotExist:
+        ip = None
+    return Response({'ip':ip})
 
 
-def db(request):
 
-    greeting = Greeting()
-    greeting.save()
 
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+def updip(request):
+    store = None
+    if IPStore.objects.exists():
+        store = IPStore.objects.get()
+    else:
+        store = IPStore()
+    store.ip = request.META['HTTP_X_FORWARDED_FOR']['origin'][-1]
+    store.save()
+    return Response({'status':'ok'})
